4cf3b847becee0fb19871322193d1ee4b8931078
josh <aaronbaon1@gmail.com>
2024-08-23 01:57:04 +0800

    added existing

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const http = require('path');
-
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req, res, next) => {
-    
-});
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(shopRoutes);
 
 
 app.listen(3000);
---
+++ controllers/shop.js
@@ -0,0 +1,13 @@
+exports.shop = (req, res) => {
+    Product.findAll()
+    .then((products) => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
